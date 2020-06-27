0c8ebc644956f81e93fd8843edca92f1723877d3
Ivan Pakpahan <67084190+ivanpakpahan27@users.noreply.github.com>
2020-06-27 12:11:58 +0700

    .

---
+++ README.md
@@ -0,0 +1,2 @@
+# Administrasi Data Pasien IGD
+File ini nanti berisi data dan source code tugas akhir RPL.
---
+++ src/Execute/ExecuteData.java
@@ -0,0 +1,16 @@
+package Execute;
+
+/**
+ *
+ * @author Ivan Pakpahan
+ */
+public class ExecuteData {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
