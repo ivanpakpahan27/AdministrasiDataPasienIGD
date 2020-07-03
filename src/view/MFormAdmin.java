53481a28744bba80cfa719d6bf8e55cfcc45a8ea
Ivan Pakpahan <67084190+ivanpakpahan27@users.noreply.github.com>
2020-07-04 02:43:52 +0700

    .

--- src/view/MFormAdmin.java
+++ src/view/MFormAdmin.java
@@ -11,11 +11,11 @@ import javax.swing.JOptionPane;
  *
  * @author Ivan Pakpahan
  */
-public class MainFormRS extends javax.swing.JFrame {
+public class MFormAdmin extends javax.swing.JFrame {
     /**
      * Creates new form MainFormRS
      */
-    public MainFormRS() {
+    public MFormAdmin() {
         initComponents();
         setDataPasien();
         setDataDokter();
@@ -1173,20 +1173,21 @@ public class MainFormRS extends javax.swing.JFrame {
                 }
             }
         } catch (ClassNotFoundException ex) {
-            java.util.logging.Logger.getLogger(MainFormRS.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+            java.util.logging.Logger.getLogger(MFormAdmin.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         } catch (InstantiationException ex) {
-            java.util.logging.Logger.getLogger(MainFormRS.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+            java.util.logging.Logger.getLogger(MFormAdmin.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         } catch (IllegalAccessException ex) {
-            java.util.logging.Logger.getLogger(MainFormRS.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+            java.util.logging.Logger.getLogger(MFormAdmin.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         } catch (javax.swing.UnsupportedLookAndFeelException ex) {
-            java.util.logging.Logger.getLogger(MainFormRS.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+            java.util.logging.Logger.getLogger(MFormAdmin.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         }
+        //</editor-fold>
         //</editor-fold>
 
         /* Create and display the form */
         java.awt.EventQueue.invokeLater(new Runnable() {
             public void run() {
-                new MainFormRS().setVisible(true);
+                new MFormAdmin().setVisible(true);
             }
         });
     }
