140927f64b3e69dbd66eabd9e75292bc501ee5ba
Ivan Pakpahan <67084190+ivanpakpahan27@users.noreply.github.com>
2020-07-14 05:42:14 +0700

    .

--- src/view/MFormDokter.java
+++ src/view/MFormDokter.java
@@ -97,6 +97,7 @@ public class MFormDokter extends javax.swing.JFrame {
         jButton7 = new javax.swing.JButton();
         jButton6 = new javax.swing.JButton();
         PanelUtama = new javax.swing.JPanel();
+        jLabel2 = new javax.swing.JLabel();
         pnlInput = new javax.swing.JPanel();
         jLabel13 = new javax.swing.JLabel();
         jLabel14 = new javax.swing.JLabel();
@@ -131,7 +132,6 @@ public class MFormDokter extends javax.swing.JFrame {
         jLabel3 = new javax.swing.JLabel();
         jLabel4 = new javax.swing.JLabel();
         jLabel10 = new javax.swing.JLabel();
-        jLabel2 = new javax.swing.JLabel();
         jLabel11 = new javax.swing.JLabel();
         jLabel12 = new javax.swing.JLabel();
 
@@ -172,6 +172,11 @@ public class MFormDokter extends javax.swing.JFrame {
         jToolBar1.add(jButton6);
 
         PanelUtama.setLayout(new java.awt.CardLayout());
+
+        jLabel2.setFont(new java.awt.Font("Times New Roman", 1, 18)); // NOI18N
+        jLabel2.setText("Halo selamat datang...");
+        jLabel2.setVerticalAlignment(javax.swing.SwingConstants.TOP);
+        PanelUtama.add(jLabel2, "card4");
 
         jLabel13.setFont(new java.awt.Font("Tahoma", 1, 11)); // NOI18N
         jLabel13.setText("ID Pasien");
@@ -474,11 +479,6 @@ public class MFormDokter extends javax.swing.JFrame {
         );
 
         PanelUtama.add(pnlProfil, "cardProfil");
-
-        jLabel2.setFont(new java.awt.Font("Times New Roman", 1, 18)); // NOI18N
-        jLabel2.setText("Halo selamat datang...");
-        jLabel2.setVerticalAlignment(javax.swing.SwingConstants.TOP);
-        PanelUtama.add(jLabel2, "card4");
 
         jLabel11.setFont(new java.awt.Font("Times New Roman", 1, 14)); // NOI18N
         jLabel11.setText("ID :");
